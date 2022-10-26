6a7d1a97e26f0c0051fa6d8f1977370c2b819482
Przemek <93125773+HaySsor@users.noreply.github.com>
2022-10-26 13:49:00 +0200

    start new projest to consolidate knowledge on first bulid app and 2 components questions and result and styled it

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="ctr">
+    <Questions
+      v-if="questionsAnswers < questions.length"
+      :questions="questions" />
+    <Result v-else />
+    <button type="button" class="reset-btn">Reset</button>
+  </div>
+</template>
+
+<script>
+import Questions from './components/Questions.vue';
+import Result from './components/Result.vue';
+import data from './data.json';
+
+export default {
+  name: 'App',
+  components: {Questions, Result},
+  data() {
+    return {
+      questions: data.questions,
+      results: data.results,
+      questionsAnswers: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
