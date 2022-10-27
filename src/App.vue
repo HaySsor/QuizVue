3c9659078528b385e2ab0dabb75cb33211c6d73d
Przemek <93125773+HaySsor@users.noreply.github.com>
2022-10-27 11:00:20 +0200

    now you can choose how may question you want, change style and first loading look

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,56 @@
 <template>
-  <div class="app-body" v-if="loadedData">
-    <div class="progress">
-      <div class="bar" :style="progresBar"></div>
-      <div class="status">
-        {{ questionsAnswers }} out of {{ questions.length }} questions answered
-      </div>
-    </div>
-    <div class="ctr">
-      <Transition name="fade" mode="out-in">
-        <Questions
-          v-if="questionsAnswers < questions.length"
-          :questions="questions"
-          :questionsAnswersd="questionsAnswers"
-          @question-answered="answeredQuestion" />
-
-        <Result
-          v-else
-          :totalCorrent="totalCorrent"
-          :questions="questions"
-          :user="user" />
-      </Transition>
-      <button
-        v-if="questionsAnswers === questions.length"
-        type="button"
-        class="reset-btn"
-        @click.prevent="reset">
-        Reset
-      </button>
+  <h1>Test your knowledge 🤔</h1>
+  <div v-if="!loadedData">
+    <label>How many Question you want ?</label>
+    <div class="option">
+      <input v-model="howManyQuestion" class="input-number" type="number" />
+      <button @click="loadHowManyQuestion">Let's Go</button>
     </div>
   </div>
-  <LoadingDataSpinner v-else />
+  <div v-else>
+    <div class="app-body" v-if="fetchDataloading">
+      <div class="progress">
+        <div class="bar" :style="progresBar"></div>
+        <div class="status">
+          {{ questionsAnswers }} out of {{ questions.length }} questions
+          answered
+        </div>
+      </div>
+      <div class="ctr">
+        <Transition name="fade" mode="out-in">
+          <Questions
+            v-if="questionsAnswers < questions.length"
+            :questions="questions"
+            :questionsAnswersd="questionsAnswers"
+            @question-answered="answeredQuestion" />
+
+          <Result
+            v-else
+            :totalCorrent="totalCorrent"
+            :questions="questions"
+            :user="user" />
+        </Transition>
+        <button
+          v-if="questionsAnswers === questions.length"
+          type="button"
+          class="reset-btn"
+          @click.prevent="reset">
+          Reset
+        </button>
+      </div>
+    </div>
+    <LoadingDataSpinner v-else />
+  </div>
 </template>
 
 <script>
 import Questions from './components/Questions.vue';
 import Result from './components/Result.vue';
-import LoadingDataSpinner from './components/LoadingDataSpinner.vue'
+import LoadingDataSpinner from './components/LoadingDataSpinner.vue';
 
 export default {
   name: 'App',
-  components: {Questions, Result,LoadingDataSpinner},
+  components: {Questions, Result, LoadingDataSpinner},
   data() {
     return {
       questions: [],
@@ -47,15 +58,18 @@ export default {
       questionsAnswers: 0,
       user: [],
       loadedData: false,
+      howManyQuestion: 1,
+      fetchDataloading: false,
     };
   },
   computed: {
     progresBar() {
       let color = '#ff63738e';
-      if (this.questionsAnswers === 2) {
-        color = '#b4bc0e8e';
-      } else if (this.questionsAnswers === 3) {
-        color = '#0de5148e';
+      if (this.questionsAnswers >= (this.questions.length / 2)){
+        color = '#b4bc0e8e'
+      }
+      if(this.questionsAnswers  == this.howManyQuestion  ){
+        color = 'lightgreen'
       }
       return {
         width: `${(this.questionsAnswers / this.questions.length) * 100}%`,
@@ -77,19 +91,25 @@ export default {
       });
       this.questionsAnswers++;
     },
+    async loadHowManyQuestion() {
+      this.loadedData = true;
+      await this.fetchData(this.howManyQuestion);
+    },
     async reset() {
       this.questionsAnswers = 0;
       this.totalCorrent = 0;
       this.user = [];
       this.loadedData = false;
+      this.howManyQuestion = 1,
+      this.fetchDataloading = false
       await this.fetchData();
     },
-    async fetchData() {
-      const URL = 'https://opentdb.com/api.php?amount=3&type=multiple';
+    async fetchData(number) {
+      const URL = `https://opentdb.com/api.php?amount=${number}&type=multiple`;
       try {
         const res = await fetch(URL);
         const data = await res.json();
-        this.loadedData = true;
+        this.fetchDataloading = true
         this.questions = data.results.map((item) => {
           return {
             question: item.question,
@@ -101,9 +121,6 @@ export default {
         console.log(err);
       }
     },
-  },
-  async mounted() {
-    await this.fetchData();
   },
 };
 </script>
